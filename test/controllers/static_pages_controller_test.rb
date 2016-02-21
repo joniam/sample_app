f48d471a0c675904f92ad0af70a5003db3430e82
Jonathan Sherman <jonathansherman@gmail.com>
2016-02-21 19:55:30 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -18,5 +18,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "About | Not Twitter"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Not Twitter"
+  end
 
 end
